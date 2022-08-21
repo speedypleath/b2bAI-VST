db13650aa4cd95907cb7c1858e7ce1ce8f1d935c
speedypleath <gheorgheandrei13@gmail.com>
2022-08-21 11:00:59 +0300

    Fixed piano layout

--- plugin/GridComponent.cpp
+++ plugin/GridComponent.cpp
@@ -33,8 +33,8 @@ void GridComponent::updateNoteLineRanges(int firstKeyStartPosition)
         noteLineRanges[i]->setLength(constants::noteLineWidth);
     }
 
-    for (int i = 0; i <= 32; i++) {
-        noteRowRanges[i]->setStart(getWidth() / 32 * i);
+    for (int i = 0; i <= 31; i++) {
+        noteRowRanges[i]->setStart(getWidth() * i / 32);
         noteRowRanges[i]->setLength(getWidth() / 32);
     }
 
@@ -68,13 +68,13 @@ void GridComponent::paint(Graphics& g)
 {
     int height = getHeight();
     int width = getWidth();
-    int noteLen = width / 32;
-    int barLen = noteLen * 4;
-
     // Draw the background
-    for (int i = 1; i < 8; i += 2) {
+    for (int i = 0; i < 8; i += 2) {
         auto shadow = DropShadow(Colours::black, 1, Point<int>(0, 0));
-        auto rect = Rectangle<int>(i * barLen, 0, barLen, height);
+        auto rect = Rectangle<int>(noteRowRanges[i * 4]->getStart(),
+                                   0,
+                                   noteRowRanges[(i + 1) * 4]->getStart() - noteRowRanges[i * 4]->getStart(),
+                                   height);
         shadow.drawForRectangle(g, rect);
     }
     g.setColour(Colours::white);
@@ -93,7 +93,7 @@ void GridComponent::paint(Graphics& g)
     g.setColour(Colours::lightgrey);
     // Draw beats lines
     for (auto noteRowRange : noteRowRanges) {
-        g.fillRect(static_cast<int>(noteRowRange->getStart()), 0, 1, height);
+        g.fillRect(static_cast<int>(noteRowRange->getStart() - 1), 0, 1, height);
     }
 
     // draw notes
